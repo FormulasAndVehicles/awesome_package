85f2274ece44aa37476e9ccfc3430633a96a898b
Thies Lennart Alff <lennart.alff@gmail.com>
2023-10-16 14:08:23 +0000

    final tutorial state

--- launch/setpoint.launch.py
+++ launch/setpoint.launch.py
@@ -1,13 +1,35 @@
-from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_path
+from launch_ros.actions import Node, PushRosNamespace
+
 from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+    IncludeLaunchDescription,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description() -> LaunchDescription:
     launch_description = LaunchDescription()
 
-    node = Node(executable='setpoint_publisher.py',
-                package='awesome_package',
-                namespace='my_namespace')
-    launch_description.add_action(node)
+    arg = DeclareLaunchArgument('vehicle_name')
+    launch_description.add_action(arg)
+
+    node = Node(executable='setpoint_publisher.py', package='awesome_package')
+    group = GroupAction([
+        PushRosNamespace(LaunchConfiguration('vehicle_name')),
+        node,
+    ])
+    launch_description.add_action(group)
+
+    package_path = get_package_share_path('fav')
+    launch_path = str(package_path / 'launch/simulation.launch.py')
+    source = PythonLaunchDescriptionSource(launch_path)
+    launch_args = {'vehicle_name': LaunchConfiguration('vehicle_name')}
+    action = IncludeLaunchDescription(source,
+                                      launch_arguments=launch_args.items())
+    launch_description.add_action(action)
 
     return launch_description
--- nodes/setpoint_publisher.py
+++ nodes/setpoint_publisher.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import math
+
 import rclpy
+from hippo_msgs.msg import ActuatorSetpoint
 from rclpy.node import Node
-from hippo_msgs.msg import ActuatorControls
 
 
 class MyFirstNode(Node):
@@ -18,34 +19,37 @@ class MyFirstNode(Node):
 
         # create a publisher. we need to specify the message type and the topic
         # name. The last argument specifies the queue length
-        self.my_publisher = self.create_publisher(ActuatorControls,
-                                                  'thruster_controls', 1)
+        self.thrust_pub = self.create_publisher(ActuatorSetpoint,
+                                                'thrust_setpoint', 1)
+        self.torque_pub = self.create_publisher(ActuatorSetpoint,
+                                                'torque_setpoint', 1)
         self.timer = self.create_timer(1 / 50, self.on_timer)
 
     def on_timer(self):
-        self.publish_my_msg()
+        self.publish_setpoints()
 
-    def publish_my_msg(self):
+    def publish_setpoints(self):
         # create the message object
-        msg = ActuatorControls()
+        thrust_msg = ActuatorSetpoint()
         now = self.get_clock().now()
-        msg.header.stamp = now.to_msg()
+        thrust_msg.header.stamp = now.to_msg()
         # get the time as floating point number in seconds
         t = now.nanoseconds * 1e-9
 
-        # the list holds 8 values for the 8 thrusters of the bluerov
-        msg.control[0] = 0.2 * math.sin(t)
-        msg.control[1] = -0.2 * math.sin(t)
-        msg.control[2] = 0.2 * math.cos(t)
-        msg.control[3] = -0.2 * math.cos(t)
-        msg.control[4] = 0.4 * math.sin(t)
-        msg.control[5] = -0.4 * math.sin(t)
-        msg.control[6] = 0.4 * math.cos(t)
-        msg.control[7] = -0.4 * math.cos(t)
+        thrust_msg.x = 0.5 * math.sin(t)
+        thrust_msg.y = -0.5 * math.sin(t)
+        thrust_msg.z = 0.5 * math.cos(t)
 
-        # publish the message with the publisher we created during the object
+        torque_msg = ActuatorSetpoint()
+        torque_msg.header.stamp = now.to_msg()
+
+        torque_msg.x = 0.4 * math.sin(t)
+        torque_msg.y = -0.4 * math.sin(t)
+        torque_msg.z = 0.4 * math.cos(t)
+        # publish the messages with the publishers we created during the object
         # initialization
-        self.my_publisher.publish(msg)
+        self.thrust_pub.publish(thrust_msg)
+        self.torque_pub.publish(torque_msg)
 
 
 def main():
